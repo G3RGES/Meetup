acd949d6bd830dc7c87e8dc83c59e75720cf0bec
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-12-10 14:09:57 +0200

    Update index.js

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -12,4 +12,43 @@ function MeetupDetails() {
   );
 }
 
+export async function getStaticPaths() {
+  return {
+    //* if fallback is false, it means all params provided are the only ones available,
+    //* if fallback is true, it means there can be some params available but not provided
+    fallback: true,
+    path: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+  console.log(meetupId);
+
+  //* fetch data for a single meetup
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+        id: meetupId,
+        title: "First Meetup",
+        address: "Some street 5, 12345 Some City",
+        description: "The first meetup",
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
