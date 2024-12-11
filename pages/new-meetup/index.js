ec09c580d87cf2b12f08267029bce0c14f4baa08
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-12-11 15:52:32 +0200

    Update index.js

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,8 +1,25 @@
+import { useRouter } from "next/router";
 import NewMeetupForm from "./../../components/meetups/NewMeetupForm";
+import data from "./../../node_modules/@mongodb-js/saslprep/dist/code-points-data-browser.d";
 
 function NewMeetupPage() {
-  function addNewMeetupHandler(enteredMeetupData) {
-    console.log(enteredMeetupData);
+  const router = useRouter();
+
+  async function addNewMeetupHandler(enteredMeetupData) {
+    // console.log(enteredMeetupData);
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+
+    router.push("/");
   }
 
   return <NewMeetupForm onAddMeetup={addNewMeetupHandler} />;
