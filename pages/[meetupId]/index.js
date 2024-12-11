73883b8762ad8d4726eae77980878174b47d3c16
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-12-11 17:37:35 +0200

    Update index.js

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -75,7 +75,13 @@ export async function getStaticProps(context) {
   //* fetch data for a single meetup
   return {
     props: {
-      meetupData: selectedMeetups,
+      meetupData: {
+        id: selectedMeetups._id.toString(),
+        title: selectedMeetups.title,
+        address: selectedMeetups.address,
+        image: selectedMeetups.image,
+        description: selectedMeetups.description,
+      },
       // {
       //   image:
       //     "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
