63fd95c9aa5c35db9caebbf2af3cee504bf418a1
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-12-11 17:14:18 +0200

    Update index.js

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,14 +1,20 @@
 import { useRouter } from "next/router";
 import MeetupDetail from "../../components/meetups/MeetupDetails";
-import { MongoClient } from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
 
-function MeetupDetails() {
+function MeetupDetails(props) {
   return (
+    // <MeetupDetail
+    //   image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
+    //   title="First Meetup"
+    //   address="Some street 5, 12345 Some City"
+    //   description="the first meetup "
+    // />
     <MeetupDetail
-      image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
-      title="First Meetup"
-      address="Some street 5, 12345 Some City"
-      description="the first meetup "
+      image={props.meetupData.image}
+      title={props.meetupData.title}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
     />
   );
 }
@@ -22,6 +28,7 @@ export async function getStaticPaths() {
   const meetupsCollection = db.collection("meetups");
 
   const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
+  client.close();
 
   return {
     //* if fallback is false, it means all params provided are the only ones available,
@@ -49,19 +56,34 @@ export async function getStaticPaths() {
 
 export async function getStaticProps(context) {
   const meetupId = context.params.meetupId;
-  console.log(meetupId);
+  // console.log(meetupId);
+
+  const client = await MongoClient.connect(
+    "mongodb+srv://G3RGES:Gerges95@cluster0.6bgr7.mongodb.net/meetups?retryWrites=true&w=majority&appName=Cluster0"
+  );
+
+  const db = client.db();
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
+  const selectedMeetups = await meetupsCollection.findOne({
+    _id: ObjectId(meetupId),
+  });
+
+  client.close();
 
   //* fetch data for a single meetup
   return {
     props: {
-      meetupData: {
-        image:
-          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
-        id: meetupId,
-        title: "First Meetup",
-        address: "Some street 5, 12345 Some City",
-        description: "The first meetup",
-      },
+      meetupData: selectedMeetups,
+      // {
+      //   image:
+      //     "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+      //   id: meetupId,
+      //   title: "First Meetup",
+      //   address: "Some street 5, 12345 Some City",
+      //   description: "The first meetup",
+      // },
     },
   };
 }
